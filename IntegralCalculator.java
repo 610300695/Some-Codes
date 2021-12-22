8c30fd8c3bb4701328a1386c9fe6b907966e03a2
610300695 <96407649+610300695@users.noreply.github.com>
2021-12-22 14:55:35 +0900

    Update IntegralCalculator.java

--- IntegralCalculator.java
+++ IntegralCalculator.java
@@ -1,73 +1,64 @@
-/*
- * 提出者 21-1-037-0063 キンシンヨウ
- * 課題 13-3
- * 提出日 2021/12/21
- */
-
-/* 5つのクラスの機能をまとめ、積分計算機としての機能を実現するクラス */
+/* 对数函数积分计算器 */
 public class IntegralCalculator {
-	private int numOfGrid; //この積分計算機の精度(積分区間を何分割するかを表す)
-	private ConditionChecker checker; //対数関数の各定数や積分区間に関する条件を判定する機能を持つオブジェクトの参照
-	private LogarithmFunction2 logFunction; //対数関数を表すオブジェクトの参照
-	private Integrator2 integrator; //積分を計算する機能を持つオブジェクトの参照
-	private Display display; //対数関数式や積分式、計算結果を表示する機能を持つオブジェクトの参照
+	private int numOfGrid; //积分精度(分割数)
+	private ConditionChecker checker; //用来判断能否积分的对象(ConditionChecker类)
+	private LogarithmFunction2 logFunction; //表示对数函数的对象(LogarithmFunction2类)
+	private Integrator2 integrator; //可以计算积分的对象(Integrator2类)
+	private Display display; //表示对数函数式子和积分结果的对象(Display类)
 	
-	/*numOfGridフィールドに設定する値のみを引数とし、
-	 * checkerとdisplayフィールドはオブジェクトを生成して参照する。
-	 * logFunctionとintegratorフィールドは、
-	 * それぞれmakeLogFunctionメソッドとmakeIntegratorメソッドでオブジェクトを生成して参照する。*/
+	/* 构造函数 */
 	public IntegralCalculator(int numOfGrid) {
 		this.numOfGrid = numOfGrid;
 		this.checker = new ConditionChecker();
 		this.display = new Display();	
 	}
 	
-	/* 積分対象の対数関数を生成するためのメソッド */
+	/* 生成对数函数的方法 */
 	public void makeLogFunction() {
 		do {
-			System.out.print("x^a log(bx + c) の a, b, c の浮動小数点数(bは0以外)をスペースで区切って入力: ");
+			System.out.print("x^a log(bx + c) 的 a, b, c 的浮动小数(b ！= 0)用空格区分输入: ");
 			checker.putABC();
 		}while (checker.isBzero());
 		
 		logFunction = new LogarithmFunction2(checker.getA(), checker.getB(), checker.getC());
 	}
 	
-	/* 積分器を生成するためのメソッド */
+	/* 生成积分器的方法 */
 	public void makeIntegrator() {
-		checker.showConditionOfStartEndPoint();//始点あるいは終点の情報を表示
+		checker.showConditionOfStartEndPoint();//表示起点或者终点的条件
 		do {
-			System.out.print("積分の始点と終点の値(浮動小数点数)をスペースで区切って入力: ");
+			System.out.print("积分的起点和终点(浮动小数)用空格区分输入: ");
 			checker.putStartAndEndPoint();
 		}while (!checker.checkStartAndEndPoint() || !checker.checkIntegralSection());
 		
 		integrator = new Integrator2(checker.getStartPoint(), checker.getEndPoint(), logFunction);
 	}
 	
-	/* displayにメッセージを送り、対数関数の式を表示するメソッド */
+	/* 输出对数函数的式子 */
 	public void displayLogFunction() {
 		display.showExpression(logFunction.getExpression());
 		System.out.println();
 	}
 	
-	/* displayにメッセージを送り、対数関数の積分式を表示するメソッド */
+	/* 输出对数函数的积分式 */
 	public void displayIntegralForm() {
 		display.showIntegralForm(checker.getStartPoint(), checker.getEndPoint(), logFunction.getExpression());
 		System.out.println();
 	}
 	
-	/* Integratorの積分機能を呼び出して積分を計算し、返ってきた結果をdisiplayにメッセージとして送って表示するメソッド */
+	/* 计算积分并输出结果 */
 	public void go() {
 		display.showResult(integrator.sectionalMensuration(this.numOfGrid));
 		System.out.println();
 	}
 	
 	public static void main(String[] args) {
-		final int numOfGrid = 100000000; // 積分区間の分割数(積分器の精度)
+		final int numOfGrid = 100000000; // 积分器的精度
 		IntegralCalculator calculator = new IntegralCalculator(numOfGrid);
-		calculator.makeLogFunction(); // 対数関数の生成
-		calculator.displayLogFunction(); // 生成された対数関数式の表示
-		calculator.makeIntegrator(); // 積分器の生成
-		calculator.displayIntegralForm(); // 積分式の表示
-		calculator.go(); // 積分を計算し、結果を表示する。
+		calculator.makeLogFunction(); // 生成对数函数
+		calculator.displayLogFunction(); // 输出对数函数式子
+		calculator.makeIntegrator(); // 生产积分器
+		calculator.displayIntegralForm(); // 输出积分式
+		calculator.go(); // 计算积分结果并输出
 	}
 }
